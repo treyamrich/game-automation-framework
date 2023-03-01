b3d025057fc6b702b56c07a4ce6d029451a30e51
Kevin Khong <kevin79ers@gmail.com>
2023-03-01 00:34:26 -0800

    adding app navigation and menu navigation

--- swOverAllMacro.py
+++ swOverAllMacro.py
@@ -4,6 +4,10 @@ import swmacros
 import psutil 
 import Test_Files_and_helper_functions.picToCornerCoords as pic
 import pyautogui
+import random 
+import os
+import win32gui
+import win32con
 
 def check_if_app_running(app_name):
     for p in psutil.process_iter():
@@ -22,22 +26,51 @@ app_path = "C:\Program Files\Google\Play Games\Bootstrapper.exe"
 def openSW():
     # Use the Popen function to start the application
     if not check_if_app_running("Google Play Games beta.exe"):
-        subprocess.Popen(app_path)
+        os.startfile(app_path)
         time.sleep(7)
-    swmacros.changeWindows("Google Play Games beta")
+        # Find the window you want to maximize by its title
+        window = win32gui.FindWindow(None, "Google Play Games beta")
+
+        # Switch to the window and maximize it
+        if window:
+            win32gui.ShowWindow(window, win32con.SW_MAXIMIZE)
+            win32gui.SetForegroundWindow(window)
+    else:
+        print("close the gaddamn program")
+        
+    # Wait for 1 second to give you time to switch to the desired window
+    time.sleep(1)
+
     time.sleep(.3)
     swcoord = swmacros.randomPointWithinRect(pic.picToCornerCoords("images\\navigateToCaiross\swIcon.png"))
     pyautogui.moveTo(swcoord)
     time.sleep(.3)
     pyautogui.mouseDown()
     pyautogui.mouseUp()
-    time.sleep(.5)
+    time.sleep(.9)
     swPlayCoord = swmacros.randomPointWithinRect(pic.picToCornerCoords("images\\navigateToCaiross\swPlay.png"))
     pyautogui.moveTo(swPlayCoord)
     time.sleep(.3)
     pyautogui.mouseDown()
     pyautogui.mouseUp()
-    time.sleep(.5)
+    time.sleep(10)
+
+    window = win32gui.FindWindow(None, "Summoners War")
+
+    # Switch to the window and maximize it
+    if window:
+        win32gui.ShowWindow(window, win32con.SW_MAXIMIZE)
+        win32gui.SetForegroundWindow(window)
+    time.sleep(20)
+    while(True):
+        if(pic.picToCornerCoords("images\\navigateToCaiross\\notice_dont_show_again.png") != []):
+            noticeCoords = swmacros.randomPointWithinRect(pic.picToCornerCoords("images\\navigateToCaiross\\notice_dont_show_again.png"))
+            time.sleep(1)
+            randomClickType = random.choice(swmacros.clickTypes)
+            randomClickType(swmacros.randomPointWithinRect(noticeCoords))   
+        else:
+            break
+
 def runScript(scriptPath):
     # Use the Popen function to run the Python file
     p = subprocess.Popen(["python", cairossScript], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
--- testChangeFile.py
+++ testChangeFile.py
@@ -1,3 +1,6 @@
 import swmacros
+import Test_Files_and_helper_functions.picToCornerCoords as pic
 
-swmacros.changeWindows("Google Play Games beta")
+# swmacros.changeWindows("Google Play Games beta")
+
+print(pic.picToCornerCoords("images\\navigateToCaiross\swPlay.png"))
